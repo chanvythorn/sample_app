5d313d3f2f8833df3a35093d15191c3cca03a1c1
Thorn Chanvy <chanvy@chanvy-Aspire-4752.(none)>
2013-02-12 17:38:45 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2e5477e610651814a7d4c8f08c11fb22386b7dc10f214549a168dccd671872a9f16ff727bdbc3a810430f27d9a7a4c56d5d66330cc3ba30509b8ab1c557b576f'
